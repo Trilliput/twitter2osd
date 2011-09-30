b1b791b14d997176151cd25188ed8d4ffc416cdf
Trilliput <root@trilliput.ru>
2011-10-01 03:18:35 +0700

    Implemented config file handling. Need to improve the logic but it works.

--- twitter2osd.py
+++ twitter2osd.py
@@ -19,6 +19,7 @@ except ImportError:
     exit(1)
 
 from contextlib import closing
+from ConfigParser import SafeConfigParser, NoOptionError
 
 import json
 import optparse
@@ -33,12 +34,37 @@ import time
 import urllib2
 
 class Twitter2osd:
-    def __init__(self, titles):
-        self.max_id_str = None
+    _default_config = {'notification_timeout':'1000', 'debug_mode':'0', 'titles':'gtk python'}
+    
+    def __init__(self):
         self.enabled = True
-        self.notification_timeout = 1000
-        self.titles = titles
-        self.debug_mode = 1
+        self.max_id = None
+        self.config_file_name = 'conf.cfg'
+        self.path_base = os.path.abspath(os.path.dirname(__file__)) + '/'
+        self.config_parser = SafeConfigParser()
+
+
+        could_read = 0
+        try:
+            could_read = len(self.config_parser.read(self.path_base + self.config_file_name))
+        except IOError:
+            print "IO Error during reading a config file." # DEBUG
+            
+        if (could_read == 0):
+            print "Use default configs" # DEBUG
+            self._drop_configs_to_defaults()
+            try:
+                with open(self.path_base + self.config_file_name, 'w+') as fo:
+                    self.config_parser.write(fo)
+            except IOError:
+                print "IO Error during creating a config file." # DEBUG
+                print "System use default configuration" # DEBUG
+        else:
+            print "Found config file" # DEBUG
+            
+        self._take_configs()
+
+
         
         self.statusicon = gtk.StatusIcon()
         self.statusicon.set_from_file("icon.png") 
@@ -46,7 +72,6 @@ class Twitter2osd:
         self.statusicon.set_tooltip("Twitter2OSD")
 
         
-        self.path_base = os.path.abspath(os.path.dirname(__file__)) + "/"
         self.path_cache = tempfile.mkdtemp()+"/"
         self.path_cached_avatars = self.path_cache + "avatars/"
         if not os.path.isdir(self.path_cached_avatars):
@@ -73,6 +98,38 @@ class Twitter2osd:
         finally:
             self._cleanup()
         
+
+    def _drop_configs_to_defaults(self, fileobject):
+        self.config_parser.remove_section('Main') # Will not rais an exception if there is now Main section
+        self.config_parser.add_section('Main')
+        for key, value in self._default_config.items():
+            self.config_parser.set('Main', key, unicode(value))
+    
+    def _take_configs(self):
+        try:
+            self.notification_timeout = int(self.config_parser.get('Main', 'notification_timeout'))
+        except NoOptionError:
+            self.notification_timeout = int(self._default_config['notification_timeout'])
+            
+        try:
+            self.titles = unicode(self.config_parser.get('Main', 'titles',))
+        except NoOptionError:
+            self.titles = unicode(self._default_config['titles'])
+            
+        try:
+            self.debug_mode = int(self.config_parser.get('Main', 'debug_mode',))
+        except NoOptionError:
+            self.debug_mode = int(self._default_config['debug_mode'])
+            
+        print "Configs:"
+        print "\tnotification_timeout = %d"%self.notification_timeout # DEBUG
+        print "\ttitles = %s"%self.titles # DEBUG
+        print "\tdebug_mode = %d"%self.debug_mode # DEBUG
+
+    #def _load_configs_from_fileobject(self, fileobject):
+    #    self.config_parser.readfp(fileobject)
+        
+    
     def _cleanup(self):
         if os.path.isdir(self.path_cache):
             shutil.rmtree(self.path_cache)
@@ -101,11 +158,8 @@ class Twitter2osd:
 
         return self.path_cached_avatars + user_id
             
-    def notify_message(self, tweet, titles):
+    def notify_message(self, tweet):
         date, user, text, profile_image_url = [tweet[x].encode("utf8") for x in ["created_at", "from_user", "text","profile_image_url"]]
-        for title in titles:
-            if title in text:
-                break
         # os.system("notify-send --icon={path_avatar} --expire-time=100 {notify_title} {text}".format(
         #             notify_title=pipes.quote(user + ' ' + date), 
         #             text=pipes.quote(text), 
@@ -143,10 +197,10 @@ class Twitter2osd:
         if self.timer_id is not None:
             new_results = None
             try:
-                if (self.max_id_str == None):
-                    new_results = self.twitter_search(request = " OR ".join(self.titles), rpp = "1")
+                if (self.max_id == None):
+                    new_results = self.twitter_search(request = self.titles.replace(' ', ' OR '), rpp = "1")
                 else:
-                    new_results = self.twitter_search(request = " OR ".join(self.titles), since_id = self.max_id_str)
+                    new_results = self.twitter_search(request = self.titles.replace(' ', ' OR '), since_id = self.max_id)
                 if (not self.enabled):
                     self.enable()
             except urllib2.URLError, e:
@@ -160,10 +214,10 @@ class Twitter2osd:
             if new_results != None:
                 print unicode(len(new_results['results'])) + " new tweets found" # DEBUG
                 
-                self.max_id_str = new_results["max_id_str"]
+                self.max_id = new_results["max_id_str"]
                 for tweet in new_results["results"]:
                     pprint.pprint (object=tweet, indent=4) # DEBUG
-                    self.notify_message (tweet, self.titles)
+                    self.notify_message (tweet)
             return True # run again in one second
         return False # stop running again
     # END Events
@@ -177,12 +231,12 @@ if __name__ == '__main__':
     # except ImportError:
     #     pass
     
-    parser = optparse.OptionParser()
-    (options, args) = parser.parse_args()
+    # parser = optparse.OptionParser()
+    # (options, args) = parser.parse_args()
 
-    titles = args
+    # titles = args
     
-    app = Twitter2osd(titles)
+    app = Twitter2osd()
     app.main()
     
 
